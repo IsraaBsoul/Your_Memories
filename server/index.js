af51851ca4d4d3bf13495ac19bc97e9e4f3cd90c
esraa <esraabsoul2501@gmail.com>
2025-03-28 22:39:51 +0300

    the base of the project+ dependencies

---
+++ server/index.js
@@ -0,0 +1,11 @@
+//our starting server point
+//now we import the dependencies that we downloaded
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app=express();
+app.use(bodyParser.json({limit : "30mb", extended:true}))
+app.use(bodyParser.urlencoded({limit : "30mb", extended:true}))
+app.use(cors());
